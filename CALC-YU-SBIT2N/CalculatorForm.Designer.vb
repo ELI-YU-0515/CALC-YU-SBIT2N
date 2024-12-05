6616c534e2a1a3efac556c3bdbf2a58ac042780f
ELI-YU-0515 <elizaryu15@gmail.com>
2024-12-06 07:06:00 +0800

    Add project files.

---
+++ CALC-YU-SBIT2N/CalculatorForm.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn1 = New Button()
+        txtDisplay = New TextBox()
+        btn2 = New Button()
+        btnAdd = New Button()
+        btn3 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btnSubtract = New Button()
+        btn6 = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btnMultiply = New Button()
+        btn9 = New Button()
+        btn0 = New Button()
+        btnClear = New Button()
+        btnDivide = New Button()
+        btnEquals = New Button()
+        SuspendLayout()
+        ' 
+        ' btn1
+        ' 
+        btn1.BackColor = SystemColors.InactiveCaption
+        btn1.FlatAppearance.BorderSize = 0
+        btn1.FlatStyle = FlatStyle.Flat
+        btn1.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn1.Location = New Point(-1, 162)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(102, 76)
+        btn1.TabIndex = 0
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = False
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        txtDisplay.Location = New Point(-1, -2)
+        txtDisplay.Multiline = True
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.ReadOnly = True
+        txtDisplay.Size = New Size(412, 156)
+        txtDisplay.TabIndex = 3
+        ' 
+        ' btn2
+        ' 
+        btn2.BackColor = SystemColors.InactiveCaption
+        btn2.FlatAppearance.BorderSize = 0
+        btn2.FlatStyle = FlatStyle.Flat
+        btn2.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn2.Location = New Point(102, 162)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(102, 76)
+        btn2.TabIndex = 0
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = False
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.BackColor = SystemColors.InactiveCaption
+        btnAdd.FlatAppearance.BorderSize = 0
+        btnAdd.FlatStyle = FlatStyle.Flat
+        btnAdd.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btnAdd.Location = New Point(308, 162)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(102, 76)
+        btnAdd.TabIndex = 0
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = False
+        ' 
+        ' btn3
+        ' 
+        btn3.BackColor = SystemColors.InactiveCaption
+        btn3.FlatAppearance.BorderSize = 0
+        btn3.FlatStyle = FlatStyle.Flat
+        btn3.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn3.Location = New Point(205, 162)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(102, 76)
+        btn3.TabIndex = 0
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = False
+        ' 
+        ' btn4
+        ' 
+        btn4.BackColor = SystemColors.InactiveCaption
+        btn4.FlatAppearance.BorderSize = 0
+        btn4.FlatStyle = FlatStyle.Flat
+        btn4.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn4.Location = New Point(-1, 239)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(102, 76)
+        btn4.TabIndex = 0
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = False
+        ' 
+        ' btn5
+        ' 
+        btn5.BackColor = SystemColors.InactiveCaption
+        btn5.FlatAppearance.BorderSize = 0
+        btn5.FlatStyle = FlatStyle.Flat
+        btn5.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn5.Location = New Point(102, 239)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(102, 76)
+        btn5.TabIndex = 0
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = False
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.BackColor = SystemColors.InactiveCaption
+        btnSubtract.FlatAppearance.BorderSize = 0
+        btnSubtract.FlatStyle = FlatStyle.Flat
+        btnSubtract.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btnSubtract.Location = New Point(308, 239)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(102, 76)
+        btnSubtract.TabIndex = 0
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = False
+        ' 
+        ' btn6
+        ' 
+        btn6.BackColor = SystemColors.InactiveCaption
+        btn6.FlatAppearance.BorderSize = 0
+        btn6.FlatStyle = FlatStyle.Flat
+        btn6.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn6.Location = New Point(205, 239)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(102, 76)
+        btn6.TabIndex = 0
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = False
+        ' 
+        ' btn7
+        ' 
+        btn7.BackColor = SystemColors.InactiveCaption
+        btn7.FlatAppearance.BorderSize = 0
+        btn7.FlatStyle = FlatStyle.Flat
+        btn7.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn7.Location = New Point(-1, 316)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(102, 76)
+        btn7.TabIndex = 0
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = False
+        ' 
+        ' btn8
+        ' 
+        btn8.BackColor = SystemColors.InactiveCaption
+        btn8.FlatAppearance.BorderSize = 0
+        btn8.FlatStyle = FlatStyle.Flat
+        btn8.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn8.Location = New Point(102, 316)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(102, 76)
+        btn8.TabIndex = 0
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = False
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.BackColor = SystemColors.InactiveCaption
+        btnMultiply.FlatAppearance.BorderSize = 0
+        btnMultiply.FlatStyle = FlatStyle.Flat
+        btnMultiply.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btnMultiply.Location = New Point(308, 316)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(102, 76)
+        btnMultiply.TabIndex = 0
+        btnMultiply.Text = "%"
+        btnMultiply.UseVisualStyleBackColor = False
+        ' 
+        ' btn9
+        ' 
+        btn9.BackColor = SystemColors.InactiveCaption
+        btn9.FlatAppearance.BorderSize = 0
+        btn9.FlatStyle = FlatStyle.Flat
+        btn9.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn9.Location = New Point(205, 316)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(102, 76)
+        btn9.TabIndex = 0
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = False
+        ' 
+        ' btn0
+        ' 
+        btn0.BackColor = SystemColors.InactiveCaption
+        btn0.FlatAppearance.BorderSize = 0
+        btn0.FlatStyle = FlatStyle.Flat
+        btn0.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btn0.Location = New Point(-1, 393)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(102, 76)
+        btn0.TabIndex = 0
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = False
+        ' 
+        ' btnClear
+        ' 
+        btnClear.BackColor = SystemColors.InactiveCaption
+        btnClear.FlatAppearance.BorderSize = 0
+        btnClear.FlatStyle = FlatStyle.Flat
+        btnClear.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btnClear.Location = New Point(102, 393)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(102, 76)
+        btnClear.TabIndex = 0
+        btnClear.Text = "CLR"
+        btnClear.UseVisualStyleBackColor = False
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.BackColor = SystemColors.InactiveCaption
+        btnDivide.FlatAppearance.BorderSize = 0
+        btnDivide.FlatStyle = FlatStyle.Flat
+        btnDivide.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btnDivide.Location = New Point(308, 393)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(102, 76)
+        btnDivide.TabIndex = 0
+        btnDivide.Text = "X"
+        btnDivide.UseVisualStyleBackColor = False
+        ' 
+        ' btnEquals
+        ' 
+        btnEquals.BackColor = SystemColors.InactiveCaption
+        btnEquals.FlatAppearance.BorderSize = 0
+        btnEquals.FlatStyle = FlatStyle.Flat
+        btnEquals.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        btnEquals.Location = New Point(205, 393)
+        btnEquals.Name = "btnEquals"
+        btnEquals.Size = New Size(102, 76)
+        btnEquals.TabIndex = 0
+        btnEquals.Text = "="
+        btnEquals.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(408, 471)
+        Controls.Add(txtDisplay)
+        Controls.Add(btnEquals)
+        Controls.Add(btn9)
+        Controls.Add(btn6)
+        Controls.Add(btnDivide)
+        Controls.Add(btn3)
+        Controls.Add(btnMultiply)
+        Controls.Add(btnSubtract)
+        Controls.Add(btnClear)
+        Controls.Add(btnAdd)
+        Controls.Add(btn8)
+        Controls.Add(btn0)
+        Controls.Add(btn5)
+        Controls.Add(btn7)
+        Controls.Add(btn4)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnEquals As Button
+
+End Class
---
+++ CALC-YU-SBIT2N/CalculatorForm.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Dim equation As String = "" ' To store the full equation for display
+
+    Private Sub NumberButton_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+        Dim button As Button = CType(sender, Button)
+        equation &= button.Text ' Append the number to the equation
+        txtDisplay.Text = equation ' Update the display
+    End Sub
+
+    Private Sub OperatorButton_Click(sender As Object, e As EventArgs) Handles btnAdd.Click, btnSubtract.Click, btnMultiply.Click, btnDivide.Click
+        Dim button As Button = CType(sender, Button)
+        equation &= " " & button.Text & " " ' Append the operator to the equation with spaces for readability
+        txtDisplay.Text = equation ' Update the display
+    End Sub
+
+    Private Sub EqualsButton_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        Try
+            ' Use DataTable to evaluate the equation
+            Dim result As Double = Convert.ToDouble(New DataTable().Compute(equation.Replace("×", "*").Replace("÷", "/"), Nothing))
+            txtDisplay.Text = result.ToString() ' Display the result
+            equation = result.ToString() ' Update the equation with the result for further calculations
+        Catch ex As Exception
+            txtDisplay.Text = "Error" ' Handle invalid equations
+            equation = "" ' Reset equation
+        End Try
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDisplay.Text = "0" ' Reset display
+        equation = "" ' Clear the equation
+    End Sub
+
+End Class
